55fd803da23b6c7782c52f857f17d85685ecb097
Margulies <Marcos.Margulies@smith-nephew.com>
2020-01-27 15:09:43 +0100

    Added new chart version, dynamic workflow status and possibility of add/remove status via drag and drop.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ChartsModule } from "ng2-charts";
+import { SearchBarComponent } from "./search-bar/search-bar.component";
+import { ChartAreaComponent } from "./chart-area/chart-area.component";
+import { DataService } from "src/app/services/data-service.service";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ChartsModule } from 'ng2-charts';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { ChartAreaComponent } from './chart-area/chart-area.component';
-import { DataService } from 'src/app/services/data-service.service';
-import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from "@angular/router";
+import { TestchartComponent } from "./testchart/testchart.component";
+import { Ng2GoogleChartsModule } from "ng2-google-charts";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+
+const appRoutes: Routes = [
+  {
+    path: "report",
+    component: ChartAreaComponent
+  },
+  { path: "testchart", component: TestchartComponent },
+  { path: "", redirectTo: "report", pathMatch: "full" },
+  { path: "**", redirectTo: "report" }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    ChartAreaComponent
+    ChartAreaComponent,
+    TestchartComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     ChartsModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    Ng2GoogleChartsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/testchart/testchart.component.ts
@@ -0,0 +1,150 @@
+import { Component, OnInit } from "@angular/core";
+import { GoogleChartInterface } from "ng2-google-charts/google-charts-interfaces";
+import {
+  DataPointPosition,
+  BoundingBox,
+  ChartHTMLTooltip,
+  ChartMouseOverEvent
+} from "ng2-google-charts";
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
+import { ɵELEMENT_PROBE_PROVIDERS } from "@angular/platform-browser";
+import { DataService } from "../services/data-service.service";
+
+@Component({
+  selector: "app-testchart",
+  templateUrl: "./testchart.component.html",
+  styleUrls: ["./testchart.component.css"]
+})
+export class TestchartComponent implements OnInit {
+  query = "assignee=currentUser()";
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.getDataFromJIRA();
+  }
+
+  public mouseOver(event: ChartMouseOverEvent) {
+    //console.log(event, event.columnLabel, ": ", event.value);
+  }
+  onEnter(value: string) {
+    this.query = value;
+    this.getDataFromJIRA();
+  }
+
+  getDataFromJIRA() {
+    this.dataService.getDaysPerStatus(this.query).subscribe(data => {
+      console.log("tickets from jira:");
+      console.dir(data);
+      this.pretifyJiraData(data);
+      this.refreshChart();
+      console.log(this.parseSource());
+    });
+  }
+
+  usedStatus = [];
+  unusedStatus = [];
+  private datasource = [];
+
+  private parseSource(): GoogleChartInterface["dataTable"] {
+    let res: GoogleChartInterface["dataTable"] = [];
+
+    let header: Array<any> = [];
+    header.push("Keys");
+    header.push("Average");
+    this.datasource.forEach(element => {
+      header.push(element.key);
+    });
+    res.push(header);
+
+    for (let i = 0; i <= this.usedStatus.length; i++) {}
+    this.usedStatus.forEach(_status => {
+      let row: Array<any> = [];
+      let total: number = 0;
+      let control: number = 0;
+      this.datasource.forEach(_dataSource => {
+        row.push(_dataSource.data[_status] || 0);
+        total += _dataSource.data[_status] || 0;
+        control += _dataSource.data[_status] ? 1 : 0;
+      });
+      // Add Average in the beggining of the Array
+      row.unshift(total / control);
+      row.unshift(_status);
+
+      res.push(row);
+    });
+    console.log(res);
+    return res;
+  }
+
+  public chartData: GoogleChartInterface = {
+    chartType: "ComboChart",
+    dataTable: this.parseSource(),
+    //firstRowIsData: true,
+    options: {
+      title: "Cycle time by JIRA keys",
+      vAxis: { title: "Time (in days)" },
+      hAxis: { title: "Status" },
+      seriesType: "bars",
+      series: { 0: { type: "line" } }
+    }
+  };
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (
+      event.previousContainer.id === "cdk-drop-list-used" &&
+      event.previousContainer.data.length === 1
+    ) {
+      return;
+    }
+    if (event.previousContainer !== event.container) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      moveItemInArray(this.usedStatus, event.previousIndex, event.currentIndex);
+    }
+    console.log(this.usedStatus);
+
+    this.refreshChart();
+  }
+
+  refreshChart() {
+    let ccComponent = this.chartData.component;
+    ccComponent.data.dataTable = this.parseSource();
+    //let ccWrapper = ccComponent.wrapper;
+    //force a redraw
+    ccComponent.draw();
+  }
+
+  private pretifyJiraData(jiraData: any) {
+    this.datasource = [];
+    this.usedStatus = [];
+
+    jiraData.forEach(element => {
+      let localData = new Object();
+      element.statusHistory.forEach(history => {
+        // Add new status to total statuses array
+        if (this.usedStatus.indexOf(history.from) < 0) {
+          this.usedStatus.push(history.from);
+        }
+        localData[history.from] = history.transitionDurationDays;
+      });
+
+      let item = {
+        key: element.key,
+        title: element.title,
+        status: element.status,
+        data: localData
+      };
+      this.datasource.push(item);
+    });
+  }
+}
