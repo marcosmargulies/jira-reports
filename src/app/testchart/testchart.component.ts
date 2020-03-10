78f340c1ab77ad68e300251108c4c5bceee0ef3a
Mario <marioarbesu85@gmail.com>
2020-03-10 18:18:07 +0100

    Refactor and css
    Add some basic styling and refactor of some functionalities

--- src/app/testchart/testchart.component.ts
+++ src/app/testchart/testchart.component.ts
@@ -23,25 +23,24 @@ export class TestchartComponent implements OnInit {
   jiraResultNoPipe: string = '';
 
   tableHeaders: string[] = [];
+  tableHeadersLabels: any[] = [];
+  headersBasicList: string[] = ['key', 'link', 'title', 'project', 'issueType', 'status', 'resolution'];
   tableData = new MatTableDataSource();
 
   jiraUrl: string = 'https://jira.ryanair.com/browse/';
 
   queryString: string = '';
+  fileName: string = '';
 
   outputType: string = '';
   outputList: any[] = [
     {
-      value: 'date',
-      label: 'Date (YYYYMMDD)'
+      value: 'transitionDurationHours',
+      label: 'Time (h)'
     },
     {
-      value: 'dateandtime',
-      label: 'Date (YYYYMMDD HH:mm:ss)'
-    },
-    {
-      value: 'time',
-      label: 'Time'
+      value: 'fromDateTime',
+      label: 'Date (MM/DD/YYYY HH:mm:ss)'
     },
     {
       value: 'all',
@@ -82,9 +81,16 @@ export class TestchartComponent implements OnInit {
       console.log('----usedStatus');
       console.log(this.datasource);
 
-      this.createTable(data, this.usedStatus);
+      let parseData = this.parseTableData(data);
+      this.tableData = new MatTableDataSource(parseData);
+      this.tableData.paginator = this.paginator;
+      this.tableData.sort = this.sort;
+      console.log('----tableData');
+      console.log(this.tableData);
+
+      this.updateTableHeaders(data, this.usedStatus);
       this.jiraResultNoPipe = this.parseJiraResultsNoPipe();
-      
+
       this.refreshChart();
     });
   }
@@ -124,27 +130,32 @@ export class TestchartComponent implements OnInit {
     return statusList;
   }
 
-  createTable(jiraData: any, currentStatus: string[]) {
-    let parseData = this.parseTableData(jiraData);
+  updateTableHeaders(jiraData: any, currentStatus: string[]) {
     this.tableHeaders = this.getTableHeaders(jiraData, currentStatus);
-
-    this.tableData = new MatTableDataSource(parseData);
-    this.tableData.paginator = this.paginator;
-    this.tableData.sort = this.sort;
+    this.tableHeadersLabels = this.getTableHeadersLabels(jiraData, currentStatus);
 
     console.log('----tableHeaders');
     console.log(this.tableHeaders);
-    console.log('----tableData');
-    console.log(this.tableData);
   }
 
   parseJiraResultsNoPipe() {
-    let jiraResults = this.tableHeaders.join(',');
+    // let jiraResults = this.tableHeaders.join(',');
+    let jiraResults = '';
+    let headerValues = [];
+    
+    this.tableHeadersLabels.forEach(headerLabel => {
+      //jiraResults += headerLabel.label + ',';
+      jiraResults += `${headerLabel.label},`;
+      headerValues.push(headerLabel.value);
+    });  
 
     this.tableData.data.forEach(jiraDataRow => {
       jiraResults += '\n';
-      this.tableHeaders.forEach(function(header, i) {
-        jiraDataRow['title'] = (jiraDataRow['title'].indexOf(',') != -1) ? jiraDataRow['title'].split(',').join('') : jiraDataRow['title'];
+      headerValues.forEach(function(header, i) {
+        if (jiraDataRow[header] && typeof jiraDataRow[header] === 'string') {
+          // Remove commas
+          jiraDataRow[header] = (jiraDataRow[header].indexOf(',') != -1) ? jiraDataRow[header].split(',').join('') : jiraDataRow[header];
+        }
         jiraResults += (i !== 0) ? ',' : '';
         jiraResults += (jiraDataRow[header]) ? `${jiraDataRow[header]}` : '';
       });
@@ -157,7 +168,7 @@ export class TestchartComponent implements OnInit {
     let tableInformation = [];
 
     jiraData.forEach(element => {
-      let test = {
+      let newElement = {
         key: element.key,
         link: this.jiraUrl + element.key,
         title: element.title,
@@ -168,27 +179,133 @@ export class TestchartComponent implements OnInit {
       };
 
       element.statusHistory.forEach(history => {
-        test[history.from] = Math.round(history.transitionDurationHours * 100) / 100;
+        let timeLabel = 'transitionDurationHours';
+        let dateLabel = 'fromDateTime';
+
+        let timeName = history.from.replace(/ /g,'') + timeLabel.charAt(0).toUpperCase() + timeLabel.slice(1);
+        let dateName = history.from.replace(/ /g,'') + dateLabel.charAt(0).toUpperCase() + dateLabel.slice(1);
+
+        newElement[timeName] = Math.round(history[timeLabel] * 100) / 100;
+        newElement[dateName] = this.formatDate(history[dateLabel]);
       });
 
-      tableInformation.push(test);
+      tableInformation.push(newElement);
     });
 
     return tableInformation;
   }
 
+  formatDate(statusDate: string) {
+    let formatDate = new Date(statusDate);
+
+    let formatter = new Intl.DateTimeFormat('en-us', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      hour12: false
+    });
+
+    return formatter.format(formatDate);
+  }
+
   getTableHeaders(jiraData: any, currentStatus: string[]) {
-    let headersList = ['key', 'link', 'title', 'project', 'issueType', 'status', 'resolution'];
+    let headersList = this.headersBasicList.slice();
+    let headerAllList = [];
 
     jiraData.forEach(element => {
       element.statusHistory.forEach(history => {
         if (headersList.indexOf(history.from) < 0 && currentStatus.indexOf(history.from) >= 0) {
           headersList.push(history.from);
+
+          // Make each column differente and avoid duplicates
+          if (this.outputType === 'all') {
+            headerAllList.push(history.from + ' (h)');
+          }
         }
       });
     });
 
+    headersList = headersList.concat(headerAllList);
+
     return headersList;
+  }
+
+  getTableHeadersLabels(jiraData: any, currentStatus: string[]) {
+    let headersList = [];
+
+    this.headersBasicList.forEach(element => {
+      headersList.push({label:element , value:element});
+    });
+
+    let labelsList = this.generateLabels(jiraData);
+    
+    headersList = headersList.concat(labelsList.start);
+    headersList = headersList.concat(labelsList.end);
+
+    console.log('¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡');
+    console.log(headersList);
+
+    return headersList;
+  }
+
+  generateLabels(jiraData: any) {
+    let labels = {start: [], end:[]};
+    let statusListed = [];
+
+    jiraData.forEach(element => {
+      element.statusHistory.forEach(history => {
+        if (statusListed.indexOf(history.from) < 0 && this.usedStatus.indexOf(history.from) >= 0) {
+          switch (this.outputType) {
+            case 'transitionDurationHours':
+              let timeLabel = 'transitionDurationHours';
+              let timeName = history.from.replace(/ /g,'') + timeLabel.charAt(0).toUpperCase() + timeLabel.slice(1);
+
+              labels.start.push({
+                label: history.from + ' (h)',
+                value: timeName
+              });
+              break;
+
+            case 'fromDateTime':
+              let dateLabel = 'fromDateTime';
+              let dateName = history.from.replace(/ /g,'') + dateLabel.charAt(0).toUpperCase() + dateLabel.slice(1);
+
+              labels.start.push({
+                label: history.from,
+                value: dateName
+              });
+              break;
+            
+            default:
+              let timeLabelAll = 'transitionDurationHours';
+              let timeNameAll = history.from.replace(/ /g,'') + timeLabelAll.charAt(0).toUpperCase() + timeLabelAll.slice(1);
+
+              let dateLabelAll = 'fromDateTime';
+              let dateNameAll = history.from.replace(/ /g,'') + dateLabelAll.charAt(0).toUpperCase() + dateLabelAll.slice(1);
+
+              // Labels to be added at the beginning of the final array
+              labels.start.push({
+                label: history.from + ' (h)',
+                value: timeNameAll
+              });
+
+              // Labels to be added at the end of the final array
+              labels.end.push({
+                label: history.from,
+                value: dateNameAll
+              });
+              break;
+          }
+
+          statusListed.push(history.from);
+        }
+      });
+    });
+
+    return labels;
   }
 
   private parseSource(): GoogleChartInterface["dataTable"] {
@@ -245,13 +362,13 @@ export class TestchartComponent implements OnInit {
     }
   };
 
-  downloadCsv(fileContent: string[], fileName: string) {
-    let test = this.arrayToCsv.transform(fileContent, this.outputType);
+  downloadCsv(fileContent: string, fileName: string) {
     let hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(test);
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(fileContent);
     hiddenElement.target = '_blank';
     hiddenElement.download = fileName + '.csv';
     hiddenElement.click();
+    this.fileName = '';
   }  
 
   drop(event: CdkDragDrop<string[]>) {
@@ -275,7 +392,12 @@ export class TestchartComponent implements OnInit {
     console.log(this.usedStatus);
 
     this.refreshChart();
-    this.createTable(this.jiraResult, this.usedStatus);
+    this.updateTableHeaders(this.jiraResult, this.usedStatus);
+    this.jiraResultNoPipe = this.parseJiraResultsNoPipe();
+  }
+
+  updateOutput() {
+    this.updateTableHeaders(this.jiraResult, this.usedStatus);
     this.jiraResultNoPipe = this.parseJiraResultsNoPipe();
   }
 
